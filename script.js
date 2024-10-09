9402783fa2da83a6ae2c5c0b6f836d7bece3c4f5
MaryMaistrova <maistrovamariia@gmail.com>
2024-10-09 13:41:10 +0300

    made minimum

--- script.js
+++ script.js
@@ -2,43 +2,46 @@
 
 // Створи масив «Список покупок». Кожен елемент масиву є об'єктом, який містить назву продукту, кількість і куплений він чи ні, ціну за одиницю товару, сума. Написати кілька функцій для роботи з таким масивом:
 // Виводити весь список на екран таким чином, щоб спочатку йшли продукти, що ще не придбані, а потім - ті, що вже придбали.
-// Покупка продукту. Функція приймає назву продукту і відзначає його як придбаний.
 
 
 const shoppingList = [
   { product: 'Milk', quantity: 1, isBought: true, price: 1, sum: 1 },
   { product: 'Bread', quantity: 2, isBought: false, price: 1.5, sum: 3 },
   { product: 'Eggs', quantity: 4, isBought: false, price: 0.75, sum: 3 },]
-
-const sortedSL = []
-
-function notBoughtPriority() {
-  for (let i = 0; i < shoppingList.length; i++) {
-    if (shoppingList[i].isBought === false) {
-      sortedSL.push(shoppingList[i])
+  
+  const sortedSL = []
+  
+  function notBoughtPriority() {
+    for (let i = 0; i < shoppingList.length; i++) {
+      if (shoppingList[i].isBought === false) {
+        sortedSL.push(shoppingList[i])
+      }
     }
-  }
-
-  for (let i = 0; i < shoppingList.length; i++) {
-    if (shoppingList[i].isBought === true) {
-      sortedSL.push(shoppingList[i])
+    
+    for (let i = 0; i < shoppingList.length; i++) {
+      if (shoppingList[i].isBought === true) {
+        sortedSL.push(shoppingList[i])
+      }
     }
+    console.log(sortedSL)
   }
-  console.log(sortedSL)
-}
-
-// notBoughtPriority()
-
+  
+  // notBoughtPriority()
+  
+// Покупка продукту. Функція приймає назву продукту і відзначає його як придбаний.
 function purchase(product) {
-  product === shoppingList[0].product ? shoppingList[0].isBought = true : shoppingList[0].isBought = false
-  product === shoppingList[1].product ? shoppingList[1].isBought = true : shoppingList[1].isBought = false
-  product === shoppingList[2].product ? shoppingList[2].isBought = true : shoppingList[2].isBought = false
+
+  for (let i = 0; i < shoppingList.length; i++) {
+    if (shoppingList[i].product === product) {
+      shoppingList[i].isBought = true
+    }
+  }
+  console.log(shoppingList)
 }
 
-purchase('Milk')
-console.log(shoppingList)
-purchase('Bread')
-console.log(shoppingList)
+// purchase('Eggs')
+
+
 
 
 
